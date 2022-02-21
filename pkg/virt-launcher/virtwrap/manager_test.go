2e98917c818210b4edb0236c6c0d94f8eb871fa4
Or Shoval <oshoval@redhat.com>
2022-02-21 11:08:33 +0200

    virt-launcher, unit-test: Adapt to ginkgo 2.0
    The suites were missing a call to gomock.Controller.Finish(),
    which verifies calls made to mocks.
    With ginkgo v2, support in GinkgoT() was extended and now
    Finish doesn't have to be called explicitly anymore.
    The suites started to fail, as there were missing or
    uncalled methods.
    
    Signed-off-by: Or Shoval <oshoval@redhat.com>

--- pkg/virt-launcher/virtwrap/manager_test.go
+++ pkg/virt-launcher/virtwrap/manager_test.go
@@ -2203,7 +2203,6 @@ var _ = Describe("migratableDomXML", func() {
 	BeforeEach(func() {
 		ctrl = gomock.NewController(GinkgoT())
 		mockDomain = cli.NewMockVirDomain(ctrl)
-		mockDomain.EXPECT().Free()
 	})
 	It("should remove only the kubevirt migration metadata", func() {
 		domXML := `<domain type="kvm" id="1">

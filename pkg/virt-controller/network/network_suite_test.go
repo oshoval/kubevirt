4da701673d1eb5bb405e32df8bc39b3853740fc3
Or Shoval <oshoval@redhat.com>
2024-02-28 17:46:00 +0200

    IPAM for secondary networks
    Implements kubevirt support for IPAM for virtualization workloads.
    Allowing VMs to get the same IP addresses during:
    1. VM restart
    2. VM migration
    
    Signed-off-by: Or Shoval <oshoval@redhat.com>

--- pkg/virt-controller/network/network_suite_test.go
+++ pkg/virt-controller/network/network_suite_test.go
@@ -20,11 +20,68 @@
 package network_test
 
 import (
+	"fmt"
 	"testing"
 
+	"k8s.io/apimachinery/pkg/runtime/schema"
+
+	v1 "kubevirt.io/api/core/v1"
+
+	"kubevirt.io/kubevirt/pkg/virt-controller/network"
+
+	networkv1 "github.com/k8snetworkplumbingwg/network-attachment-definition-client/pkg/apis/k8s.cni.cncf.io/v1"
+	fakenetworkclient "kubevirt.io/client-go/generated/network-attachment-definition-client/clientset/versioned/fake"
+
 	"kubevirt.io/client-go/testutils"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+const (
+	nadSuffix              = "-net"
+	nsSuffix               = "-ns"
+	redNetworkLogicalName  = "red"
+	redNamespace           = redNetworkLogicalName + nsSuffix
+	redNetworkNadName      = redNetworkLogicalName + nadSuffix
+	blueNetworkLogicalName = "blue"
+	blueNamespace          = blueNetworkLogicalName + nsSuffix
+	blueNetworkNadName     = blueNetworkLogicalName + nadSuffix
+	resourceName           = "resource_name"
 )
 
 func TestNetwork(t *testing.T) {
 	testutils.KubeVirtTestSuiteSetup(t)
+}
+
+func createNADs(networkClient *fakenetworkclient.Clientset, namespace string, networks []v1.Network, persistentIPs map[string]struct{}) error {
+	gvr := schema.GroupVersionResource{
+		Group:    "k8s.cni.cncf.io",
+		Version:  "v1",
+		Resource: "network-attachment-definitions",
+	}
+	for _, net := range networks {
+		if net.Multus == nil {
+			continue
+		}
+
+		ns, networkName := network.GetNamespaceAndNetworkName(namespace, net.NetworkSource.Multus.NetworkName)
+		nad := &networkv1.NetworkAttachmentDefinition{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:        networkName,
+				Namespace:   ns,
+				Annotations: map[string]string{network.MULTUS_RESOURCE_NAME_ANNOTATION: resourceName},
+			},
+		}
+
+		if _, exists := persistentIPs[networkName]; exists {
+			nad.Spec.Config = fmt.Sprintf(`{"allowPersistentIPs": true, "name": "%s"}`, nadNetworkName)
+		}
+
+		err := networkClient.Tracker().Create(gvr, nad, ns)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }

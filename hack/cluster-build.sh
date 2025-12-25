95e57eae158573bda03ee238aecdd128863dc080
Or Shoval <oshoval@redhat.com>
2025-12-25 11:23:03 +0200

    WIP: fast deploy
    cluster-sync also runs manifests.sh and deploy-to-nodes.sh
    
    Signed-off-by: Or Shoval <oshoval@redhat.com>

--- hack/cluster-build.sh
+++ hack/cluster-build.sh
@@ -23,6 +23,12 @@ set -e
 
 DOCKER_TAG=${DOCKER_TAG:-devel}
 DOCKER_TAG_ALT=${DOCKER_TAG_ALT:-devel_alt}
+SKIP_BUILD=${SKIP_BUILD:-}
+
+if [[ -n "$SKIP_BUILD" ]]; then
+    PUSH_TARGETS=${PUSH_TARGETS:-"virt-operator virt-api virt-controller virt-handler virt-launcher virt-exportserver virt-exportproxy"}
+    DOCKER_TAG_ALT=
+fi
 
 source hack/common.sh
 source kubevirtci/cluster-up/cluster/$KUBEVIRT_PROVIDER/provider.sh
@@ -31,8 +37,12 @@ source hack/config.sh
 echo "Building ..."
 
 # Build everything and publish it
-${KUBEVIRT_PATH}hack/dockerized "BUILD_ARCH=${BUILD_ARCH} DOCKER_PREFIX=${DOCKER_PREFIX} DOCKER_TAG=${DOCKER_TAG} KUBEVIRT_PROVIDER=${KUBEVIRT_PROVIDER} ./hack/bazel-build-functests.sh"
-${KUBEVIRT_PATH}hack/dockerized "BUILD_ARCH=${BUILD_ARCH} DOCKER_PREFIX=${DOCKER_PREFIX} DOCKER_TAG=${DOCKER_TAG} DOCKER_TAG_ALT=${DOCKER_TAG_ALT} KUBEVIRT_PROVIDER=${KUBEVIRT_PROVIDER} IMAGE_PREFIX=${IMAGE_PREFIX} IMAGE_PREFIX_ALT=${IMAGE_PREFIX_ALT} ./hack/multi-arch.sh push-images"
+if [[ -z "$SKIP_BUILD" ]]; then
+    ${KUBEVIRT_PATH}hack/dockerized "BUILD_ARCH=${BUILD_ARCH} DOCKER_PREFIX=${DOCKER_PREFIX} DOCKER_TAG=${DOCKER_TAG} KUBEVIRT_PROVIDER=${KUBEVIRT_PROVIDER} ./hack/bazel-build-functests.sh"
+else
+    echo "Skipping build (SKIP_BUILD=$SKIP_BUILD)"
+fi
+${KUBEVIRT_PATH}hack/dockerized "BUILD_ARCH=${BUILD_ARCH} DOCKER_PREFIX=${DOCKER_PREFIX} DOCKER_TAG=${DOCKER_TAG} DOCKER_TAG_ALT=${DOCKER_TAG_ALT} KUBEVIRT_PROVIDER=${KUBEVIRT_PROVIDER} IMAGE_PREFIX=${IMAGE_PREFIX} IMAGE_PREFIX_ALT=${IMAGE_PREFIX_ALT} PUSH_TARGETS='${PUSH_TARGETS}' ./hack/multi-arch.sh push-images"
 BUILD_ARCH=${BUILD_ARCH} DOCKER_PREFIX=${DOCKER_PREFIX} DOCKER_TAG=${DOCKER_TAG} hack/push-container-manifest.sh
 
 echo "Done $0"
